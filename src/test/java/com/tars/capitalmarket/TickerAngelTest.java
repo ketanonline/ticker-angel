3653008d7d6bc9ff2ac7153c29d4ad1f7cd5fbd2
ketanonline <ketanonline>
2022-06-06 03:34:12 +0000

    Initial commit

---
+++ src/test/java/com/tars/capitalmarket/TickerAngelTest.java
@@ -0,0 +1,21 @@
+package com.tars.capitalmarket;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class TickerAngelTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
